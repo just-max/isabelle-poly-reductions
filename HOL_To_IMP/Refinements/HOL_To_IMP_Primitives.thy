74d323719007ca7acf8bf511232ba62aa78ddf0d
Max Lang <17551908+just-max@users.noreply.github.com>
2025-05-27 03:21:57 +0200

    first idea for zero running times

--- HOL_To_IMP/Refinements/HOL_To_IMP_Primitives.thy
+++ HOL_To_IMP/Refinements/HOL_To_IMP_Primitives.thy
@@ -192,6 +192,22 @@ definition "terminates_with_res_time_order_IMP' p r f T_f \<equiv>
       - Any change here would of course nevertheless need to be accounted for whenever an auxiliary function is called.
  *)
 
+
+definition "positive (n :: nat) \<equiv> max n 1"
+notation positive (\<open>(_\<^sup>+)\<close> [1000])
+
+definition "of_order_z g = (\<lambda>f. \<exists>c. \<forall>x. f x \<le> c * (g x)\<^sup>+)"
+(* - for a function that is nowhere-zero, this is the same as before
+   - for a function that is everywhere-zero, this says f must be less than some c everywhere
+   - in general, there must be some constant c, such that f is larger than g only up to the factor c,
+      except where g is zero, where f is no larger than just c
+
+  TODO: use this new definition, needs some thought into how to restructure *)
+
+definition "terminates_with_res_time_order_IMP'_z p r f T_f \<equiv>
+  \<exists>T. (of_order_z T_f) T \<and> (\<forall>s. terminates_with_res_time_IMP p s r (f s) (T s))"
+
+
 (* TODO: we may need to add a predicate on states where they are hidden inside a definition *)
 
 
